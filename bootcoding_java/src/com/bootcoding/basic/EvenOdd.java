106ad8b75740a7e172a71b0f5d4d5d4fea39f113
SwarajBhalerao <swarajbhalerao2001@gmail.com>
2022-08-18 20:59:37 +0530

    Even Odd

---
+++ bootcoding_java/src/com/bootcoding/basic/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class EvenOdd {
+     public static void main(String args[]){
+         int num=10;
+         if(num%2==0){
+             System.out.println("even");
+         }
+         else{
+             System.out.println("odd");
+         }
+     }
+}
+
